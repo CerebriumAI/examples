e9710dc104afe086eafb5645eaef4c6c452d498b
Michael Louis <michaellouis157@gmail.com>
2023-04-12 17:23:26 -0400

    Added HF embeddings and flant5 model

--- 3. Langchain/main.py
+++ 3. Langchain/main.py
@@ -1,16 +1,22 @@
 import pytube 
 from pydantic import BaseModel
 from typing import Optional
-from models.common.decorators import worker
+import whisper
 from datetime import datetime
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.chains import VectorDBQAWithSourcesChain
-from langchain import OpenAI
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CerebriumAI
+from sentence_transformers import SentenceTransformer
 import requests
 import faiss
+import os
 
+model = whisper.load_model("small")
+sentanceTransformer = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+os.environ["CEREBRIUMAI_API_KEY"] = "c_api_key-3f1bc409f7e9e6103cdd554e3737a870c445499f"
 
 class Item(BaseModel):
     url: str
@@ -35,16 +41,35 @@ def store_segments(segments):
 
   return texts, start_times
 
-@worker
-def predict(item: Item):
+def create_embeddings(texts, start_times):
+    text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
+    docs = []
+    metadatas = []
+    for i, d in enumerate(texts):
+        splits = text_splitter.split_text(d)
+        docs.extend(splits)
+        metadatas.extend([{"source": start_times[i]}] * len(splits))
+    return metadatas, docs
 
-    video = pytube.YouTube(url)
+def predict(item: Item, run_id, logger):
+
+    video = pytube.YouTube(item.url)
     video.streams.get_highest_resolution().filesize
     audio = video.streams.get_audio_only()
     fn = audio.download(output_path="/content/clips/")
 
-    model = whisper.load_model("small")
     transcription = model.transcribe('/content/tmp.mp3')
     res = transcription['segments']
 
     texts, start_times = store_segments(res)
+
+    metadatas, docs = create_embeddings(texts, start_times)
+    embeddings = HuggingFaceEmbeddings()
+    store = FAISS.from_texts(docs, embeddings, metadatas=metadatas)
+    faiss.write_index(store.index, "docs.index")
+    llm = CerebriumAI(endpoint_url="https://run.cerebrium.ai/flan-t5-xl-webhook/predict")
+    chain = VectorDBQAWithSourcesChain.from_llm(llm=llm, vectorstore=store)
+
+    result = chain({"question": item.question})
+
+    return {"result": result}
