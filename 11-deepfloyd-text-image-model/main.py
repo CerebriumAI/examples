2eb964839537ce54145ef5c96d42245697be10f8
Michael Katsoulis <michaelkatsoulis@gmail.com>
2023-05-03 18:59:57 +0200

    Add API token for verification

--- 11-deepfloyd-text-image-model/main.py
+++ 11-deepfloyd-text-image-model/main.py
@@ -1,8 +1,9 @@
 import base64
-import io
+from io import BytesIO
 from diffusers import DiffusionPipeline
 from diffusers.utils import pt_to_pil
 import torch
+import requests
 
 from pydantic import BaseModel
 
@@ -13,34 +14,44 @@ from pydantic import BaseModel
 ########################################################################
 class Item(BaseModel):
     prompt : str
+    # image:  Optional[]
+    # url:  Optional[str]
 
 
 ########################################################################
 # Run once on first deployment to initialise the model
 ########################################################################
 
+access_token = '<<<<Your Access Token Here>>>>'
+
 # stage 1
 stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16",
-                                             torch_dtype=torch.float16)
+                                             torch_dtype=torch.float16, use_auth_token=access_token)
 # stage_1.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 # stage_1.enable_model_cpu_offload()
 stage1 = stage_1.to("cuda")
 
 
 # stage 2
-stage_2 = DiffusionPipeline.from_pretrained(
-    "DeepFloyd/IF-II-L-v1.0", text_encoder=None, variant="fp16", torch_dtype=torch.float16)
+stage_2 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-II-L-v1.0", text_encoder=None,
+                                             variant="fp16", torch_dtype=torch.float16, use_auth_token=access_token)
 # stage_2.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 # stage_2.enable_model_cpu_offload()
 stage2 = stage_2.to("cuda")
 
 # stage 3
 safety_modules = {"feature_extractor": stage_1.feature_extractor, "safety_checker": stage_1.safety_checker, "watermarker": stage_1.watermarker}
-stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler", **safety_modules, torch_dtype=torch.float16)
+stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler",
+                                             **safety_modules, torch_dtype=torch.float16, use_auth_token=access_token)
 # stage_3.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 # stage_3.enable_model_cpu_offload()
 stage3 = stage_3.to("cuda")
 
+
+
+# def download_image(image):
+#     r = requests.get(image)
+#     return Image.open(BytesIO(r.content))
 
 
 
@@ -77,7 +88,7 @@ def predict(item, run_id, logger):
 
     # Converting and returning the finished image.
     logger.info('Converting')
-    buffered = io.BytesIO()
+    buffered = BytesIO()
     image[0].save(buffered, format="PNG")
     finished_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
