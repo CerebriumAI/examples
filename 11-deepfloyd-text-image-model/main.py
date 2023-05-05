569e4b2d77beaf4a616d452bd463a370fe318608
Michael Katsoulis <michaelkatsoulis@gmail.com>
2023-05-05 13:57:19 +0200

    cleanup comments

--- 11-deepfloyd-text-image-model/main.py
+++ 11-deepfloyd-text-image-model/main.py
@@ -26,24 +26,18 @@ access_token = '<<<<Your Access Token Here>>>>'
 # stage 1
 stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16",
                                              torch_dtype=torch.float16, use_auth_token=access_token)
-# stage_1.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
-# stage_1.enable_model_cpu_offload()
 stage1 = stage_1.to("cuda")
 
 
 # stage 2
 stage_2 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-II-L-v1.0", text_encoder=None,
                                              variant="fp16", torch_dtype=torch.float16, use_auth_token=access_token)
-# stage_2.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
-# stage_2.enable_model_cpu_offload()
 stage2 = stage_2.to("cuda")
 
 # stage 3
 safety_modules = {"feature_extractor": stage_1.feature_extractor, "safety_checker": stage_1.safety_checker, "watermarker": stage_1.watermarker}
 stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler",
                                              **safety_modules, torch_dtype=torch.float16, use_auth_token=access_token)
-# stage_3.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
-# stage_3.enable_model_cpu_offload()
 stage3 = stage_3.to("cuda")
 
 
