109d54d68883d838549dd837d4f6ce9d927796d1
Michael Katsoulis <michaelkatsoulis@gmail.com>
2023-09-30 13:43:41 +0300

    Example for first corted deployment

---
+++ first-cortex-deployment/main.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from pydantic import BaseModel
+import numpy as np # import a library you've entered in your  requirements.txt
+
+class Item(BaseModel):
+    # Add your input parameters here
+    prompt: str
+    your_optional_param: Optional[str] = None # an example optional parameter
+
+### Setup your model here. 
+"""
+This code will be run once when your replica starts. 
+Load your model, setup datasets, pull in secrets, etc. here
+"""
+
+
+def predict(params, run_id, logger):
+    params = Item(**params)
+    # This code is run on every inference inference request.
+
+    # Access the parameters from your inference request
+    prompt = params.prompt
+    logger.info(f"Recieved a prompt of: `{prompt}`")
+    
+    if params.your_optional_param is not None:
+        logger.info(f"You sent an optional param of: {params.your_optional_param}")
+    
+    
+    ### ADD YOUR CODE HERE
+
+    return {"your_prompt": params.prompt, "your_other_return": "success"} # return your results 
+
+    """
+    To deploy your model, run:
+    cerebrium deploy <<YOUR DEPLOYMEN TNAME>> --config-file config.yaml
+    """
