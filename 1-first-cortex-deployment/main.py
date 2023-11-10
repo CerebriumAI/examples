e4c53093fac190a9ebd0cad47760263b927e7407
Jonathan Irwin <jonoirwinrsa@gmail.com>
2023-11-09 21:27:57 -0500

    lint: spelling and linting

--- 1-first-cortex-deployment/main.py
+++ 1-first-cortex-deployment/main.py
@@ -1,13 +1,15 @@
 from typing import Optional
+
 from pydantic import BaseModel
-import numpy as np # import a library you've entered in your  requirements.txt
+
 
 class Item(BaseModel):
     # Add your input parameters here
     prompt: str
-    your_optional_param: Optional[str] = None # an example optional parameter
+    your_optional_param: Optional[str] = None  # an example optional parameter
 
-### Setup your model here. 
+
+### Setup your model here.
 """
 This code will be run once when your replica starts. 
 Load your model, setup datasets, pull in secrets, etc. here
@@ -16,21 +18,23 @@ Load your model, setup datasets, pull in secrets, etc. here
 
 def predict(item, run_id, logger):
     params = Item(**item)
-    # This code is run on every inference inference request.
+    # This code is run on every inference request.
 
     # Access the parameters from your inference request
     prompt = params.prompt
     logger.info(f"Received a prompt of: `{prompt}`")
-    
+
     if params.your_optional_param is not None:
         logger.info(f"You sent an optional param of: {params.your_optional_param}")
-    
-    
+
     ### ADD YOUR CODE HERE
 
-    return {"your_prompt": params.prompt, "your_other_return": "success"} # return your results 
+    return {
+        "your_prompt": params.prompt,
+        "your_other_return": "success",
+    }  # return your results
 
     """
     To deploy your model, run:
-    cerebrium deploy <<YOUR DEPLOYMEN TNAME>> --config-file config.yaml
+    cerebrium deploy <<YOUR DEPLOYMENT NAME>> --config-file config.yaml
     """
--- 5-loading-model-weights-faster/fast_load.py
+++ 5-loading-model-weights-faster/fast_load.py
@@ -1,30 +1,36 @@
-import time
 import os
 import sys
+import time
+
 from transformers import AutoModelForCausalLM, AutoConfig
 
-
 # Setup the paths to the model cache
-APP_NAME = os.environ.get("APP_NAME", "default") # this gets the name of your deployment from the environment variables
+APP_NAME = os.environ.get(
+    "APP_NAME", "default"
+)  # this gets the name of your deployment from the environment variables
 private_model_path = f"/persistent-storage/.cache/tensorised-models/{APP_NAME}/{{org}}/{{model_name}}.tensors"
+
 
 def fast_load(model_id, load_weights_func, faster=False):
     """A function that loads a model from the cache if it exists, or serialises it if it doesn't
     Args:
         model_id (str): The model id in the form of org/model_name
         load_weights_func (function): Some function where you load your model, send it to GPU and prep it for inference
-        faster (bool, optional): Whether to use the faster method of loading the. Defaults to False."""
+        faster (bool, optional): Whether to use the faster method of loading the. Defaults to False.
+    """
     org, model_name = model_id.split("/")
     model_path = private_model_path.format(org=org, model_name=model_name)
 
     # Check if the model exists in the cache
     if os.path.isfile(model_path):
-        print((f"Deserialising model from {model_path}"),  file=sys.stderr) # printing to sterr prints the output immediately. Otherwise it will be buffered.
+        print(
+            f"Deserialising model from {model_path}", file=sys.stderr
+        )  # printing to stderr prints the output immediately. Otherwise, it will be buffered.
         model = deserialise_saved_model(model_path, model_id, plaid=faster)
     else:
         # some function where you load your model, send it to GPU and prep it for inference
         model = load_weights_func()
-        print((f"Serialising model to {model_path}"),  file=sys.stderr)
+        print(f"Serialising model to {model_path}", file=sys.stderr)
         serialise_model(model, model_path)
     return model
 
@@ -34,12 +40,11 @@ def deserialise_saved_model(model_path, model_id, plaid=True):
     from tensorizer import TensorDeserializer
     from tensorizer.utils import no_init_or_tensor
 
-
     # create a config object that we can use to init an empty model
     config = AutoConfig.from_pretrained(model_id)
 
     # Init an empty model without loading weights into gpu. We'll load later.
-    print(("Initialising empty model"),  file=sys.stderr)
+    print("Initialising empty model", file=sys.stderr)
     start = time.time()
     with no_init_or_tensor():
         # Load your model here using whatever class you need to initialise an empty model from a config.
@@ -53,15 +58,15 @@ def deserialise_saved_model(model_path, model_id, plaid=True):
     deserializer = TensorDeserializer(model_path, plaid_mode=True)
 
     # Deserialise the model straight into GPU (zero-copy)
-    print(("Loading model"),  file=sys.stderr)
+    print("Loading model", file=sys.stderr)
     start = time.time()
     deserializer.load_into_module(model)
     end = time.time()
     deserializer.close()
 
     # Report on the timings.
-    print(f"Initialising empty model took {end_init} seconds",  file=sys.stderr)
-    print((f"\nDeserialising model took {end - start} seconds\n"),  file=sys.stderr)
+    print(f"Initialising empty model took {end_init} seconds", file=sys.stderr)
+    print(f"\nDeserialising model took {end - start} seconds\n", file=sys.stderr)
 
     return model
 
@@ -69,14 +74,15 @@ def deserialise_saved_model(model_path, model_id, plaid=True):
 def serialise_model(model, model_path):
     """Serialise the model and saving the weights to the model_path"""
     from tensorizer import TensorSerializer
+
     try:
         serializer = TensorSerializer(model_path)
         start = time.time()
         serializer.write_module(model)
         end = time.time()
-        print((f"Serialising model took {end - start} seconds"),  file=sys.stderr)
+        print(f"Serialising model took {end - start} seconds", file=sys.stderr)
         serializer.close()
         return True
     except Exception as e:
-        print("Serialisation failed with error: ", e,  file=sys.stderr)
+        print("Serialisation failed with error: ", e, file=sys.stderr)
         return False
