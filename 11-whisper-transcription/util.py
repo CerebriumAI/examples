2d534f623d5436b7448d0110ff8b2acd6d94448c
Kyle Gani <kylegani@Kyles-MacBook-Pro-3.local>
2024-05-10 18:24:28 +0200

    Updated: examples

--- 11-whisper-transcription/util.py
+++ 11-whisper-transcription/util.py
@@ -1,18 +1,15 @@
 import base64
 import uuid
+import requests
 
 DOWNLOAD_ROOT = "/tmp/"  # Change this to /persistent-storage/ if you want to save files to the persistent storage
 
-
-# Downloads a file from a given URL and saves it to a given filename
-def download_file_from_url(logger, url: str, filename: str):
-    logger.info("Downloading file...")
-
-    import requests
+def download_file_from_url(url: str, filename: str):
+    print("Downloading file...")
 
     response = requests.get(url)
     if response.status_code == 200:
-        logger.info("Download was successful")
+        print("Download was successful")
 
         with open(filename, "wb") as f:
             f.write(response.content)
@@ -20,13 +17,13 @@ def download_file_from_url(logger, url: str, filename: str):
         return filename
 
     else:
-        logger.info(response)
+        print(response)
         raise Exception("Download failed")
 
 
 # Saves a base64 encoded file string to a local file
-def save_base64_string_to_file(logger, audio: str):
-    logger.info("Converting file...")
+def save_base64_string_to_file(audio: str):
+    print("Converting file...")
 
     decoded_data = base64.b64decode(audio)
 
@@ -35,5 +32,6 @@ def save_base64_string_to_file(logger, audio: str):
     with open(filename, "wb") as file:
         file.write(decoded_data)
 
-    logger.info("Decoding base64 to file was successful")
+    print("Decoding base64 to file was successful")
     return filename
+
