839a31adecc5d9d1d9d6bbb276406f5bdce904e0
Michael Louis <michaellouis157@gmail.com>
2024-05-14 17:32:37 -0400

    Added inferentia exampls

---
+++ 14-inferentia/vllm/main.py
@@ -0,0 +1,68 @@
+
+from typing import Optional
+from pydantic import BaseModel
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+import time
+import os
+from huggingface_hub import login
+
+os.environ['NEURON_COMPILE_CACHE_URL'] = '/persistent-storage/neuron-compile-cache'
+
+
+class Item(BaseModel):
+    prompts: list[str] = []
+    temperature: Optional[float] = 0.8
+    top_p: Optional[float] = 0.95
+
+login(token = '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')
+
+model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+llm = LLM(
+    model=model_id,
+    max_num_seqs=1,
+    max_model_len=128,
+    block_size=128,
+    # The device can be automatically detected when AWS Neuron SDK is installed.
+    # The device argument can be either unspecified for automated detection,
+    # or explicitly assigned.
+    device="neuron",
+    tensor_parallel_size=2)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
+def predict(
+        prompts,
+        temperature = 0.8,
+        top_p = 0.95
+    ):
+    item = Item(prompts=prompts, temperature=temperature, top_p=top_p)
+
+    # Start timing
+    start_time = time.time()
+
+    sampling_params = SamplingParams(temperature=item.temperature, top_p=item.top_p)
+    outputs = llm.generate(item.prompts, sampling_params)
+
+    # End timing
+    end_time = time.time()
+
+    total_tokens = 0
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+        # Assuming you have a way to count tokens, e.g., a function count_tokens(text)
+        total_tokens += count_tokens(prompt) + count_tokens(generated_text)
+
+    # Calculate duration and tokens per second
+    duration = end_time - start_time
+    tokens_per_second = total_tokens / duration if duration > 0 else 0
+
+    print(f"Processed {total_tokens} tokens in {duration:.2f} seconds ({tokens_per_second:.2f} tokens/second)")
+
+    return {"outputs": outputs, "tokens_per_second": tokens_per_second}
+
+def count_tokens(text):
+    return len(tokenizer.tokenize(text))
