b84a1901d9970ffc92930846437aac87f27995bf
Michael Louis <michaellouis157@gmail.com>
2024-11-25 09:26:09 -0500

    update request structure

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,11 @@ function App() {
           'Content-Type': 'application/json',
           'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiJwLTJlNDE1ZDA1IiwiaWF0IjoxNzMyNTM4MjEzLCJleHAiOjIwNDgxMTQyMTN9.4yVrto4fuLtMeoNOTZGOH8d1crY-Qx2sD-pX29Jgh42j_z6ZIV5Qwr1YcFN5_ExU8hcTs6tRQ0kB6yXRsDisgAmHBGBJG0SnnmAanO_eVnt2fw5R93JhWMeDjLpCjsSIWKaiqKwA28Gos_0aMboK9-jRsV9aurhHaIX7l0BnW7kYU_YgOSWPeH92tFgoaCj1pvH6f00uorx429-dY5ynFFBfY3V23kRRMQgrz7rZ2agvq5FRCLzbTgOBJmDJGKNPzzFhtVhORpMrJM1xa5ING_1A2doTFN1vQNoHKZ6AnN_THQrXxVe7f4FeJNLFYXoRsixYRrrIFfR-cf8u8VymJQ`
         },
-        body: JSON.stringify({
+        body: JSON.stringify({request: {
           url: repoUrl,
           provider,
           token,
-        }),
+        }}),
       });
 
       const reader = res.body?.getReader();
